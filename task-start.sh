4de305d0926f481f5d8134dbb0349e78273644fc
IPnett Cloud Services <team@cloud.ipnett.com>
2016-02-18 17:36:12 +0100

    improved

--- task-start.sh
+++ task-start.sh
@@ -4,6 +4,11 @@ TASK=$1
 
 TEMP_TASK=task.$$.yaml.tmp
 
-cat variables.txt $TASK > $TEMP_TASK
-rally task start $TEMP_TASK  2>/dev/null
-#rm -f $TEMP_TASK
+if [ -f $TASK ]; then
+	cat variables.txt $TASK > $TEMP_TASK
+	rally task start $TEMP_TASK  2>/dev/null
+	rm -f $TEMP_TASK
+else
+	echo "$TASK not found"
+	exit 1
+fi
