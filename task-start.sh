6946e821a2daa87a663037fd0f9e8d7c98ac38b5
IPnett Cloud Services <team@cloud.ipnett.com>
2016-02-19 09:23:00 +0100

    use proper args file, generate reports

--- task-start.sh
+++ task-start.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
 TASK=$1
+ARGS=${2:-args.yaml}
 
-TEMP_TASK=task.$$.yaml.tmp
+WEBROOT=/var/www/html
+TIMESTAMP=`date +%Y%m%d_%H%M%S`
+TASKNAME=`echo $TASK | sed -e s,scenarios/,, -e s,/,_,g -e s,.yaml,,`
 
-if [ -f $TASK ]; then
-	cat variables.txt $TASK > $TEMP_TASK
-	rally task start $TEMP_TASK  2>/dev/null
-	rm -f $TEMP_TASK
-else
-	echo "$TASK not found"
-	exit 1
-fi
+HTML_REPORT=${WEBROOT}/${TIMESTAMP}_${TASKNAME}.html
+JUNIT_REPORT=${WEBROOT}/${TIMESTAMP}_${TASKNAME}.xml
+
+rally task start --task-args-file $ARGS $TASK 2>/dev/null
+
+rally task report --html-static --out $HTML_REPORT
+rally task report --junit --out $JUNIT_REPORT
