746031b0ccd890c45606c23d9f5589fed4ceb82e
Jakob Schlyter <jakob@kirei.se>
2016-02-19 10:36:52 +0100

    improve

--- task-suite.sh
+++ task-suite.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+SCENARIOS=scenarios.txt
+
 SUITE=$1
 ARGS=${2:-args.yaml}
+
+grep ^scenarios/$SUITE/ $SCENARIOS |  xargs python yaml-merge.py > scenarios/$SUITE.yaml
+TASK=scenarios/$SUITE.yaml
 
 WEBROOT=/var/www/html
 TIMESTAMP=`date +%Y%m%d_%H%M%S`
@@ -9,10 +14,6 @@ TASKNAME=$SUITE
 
 HTML_REPORT=${WEBROOT}/${TIMESTAMP}_${TASKNAME}.html
 JUNIT_REPORT=${WEBROOT}/${TIMESTAMP}_${TASKNAME}.xml
-
-find scenarios/$SUITE -type f -name \*.yaml -print | xargs python yaml-merge.py > scenarios/$SUITE.yaml
-
-TASK=scenarios/$SUITE.yaml
 
 rally task start --task-args-file $ARGS $TASK 2>/dev/null
 
